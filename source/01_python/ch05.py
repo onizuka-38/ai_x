a24c1744f9755de9932585de466f8b69205a73a3
onizuka-38 <bdh9938@gmail.com>
2025-04-16 18:02:33 +0900

    함수

---
+++ source/01_python/ch05.py
@@ -0,0 +1,10 @@
+# 함수 정의 : 4월 16일(수)
+def my_hello():
+    print('Hello, Python')
+    print('Hello, World')
+def my_hello(cnt):
+    for i in range(cnt):
+        print('Hello, Python')
+        print('Hello, World')
+if __name__ == "__main__":
+    my_hello(3)
