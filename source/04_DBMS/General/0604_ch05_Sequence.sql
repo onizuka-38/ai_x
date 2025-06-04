98441f3e01f9c6ac67a87cc85fad2382930ec4a2
onizuka-38 <bdh9938@gmail.com>
2025-06-04 15:44:07 +0900

    oracle02

---
+++ source/04_DBMS/General/0604_ch05_Sequence.sql
@@ -0,0 +1,27 @@
+-- [V] Sequence : 순차번호 생성기. 대부분 인위적인 PK 사용용도
+DROP SEQUENCE FRIEND_SEQ;
+CREATE SEQUENCE FRIEND_SEQ
+	START WITH 1 -- 1부터 시작(기본값)
+	INCREMENT BY 1 -- 1씩 증가(기본값)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
