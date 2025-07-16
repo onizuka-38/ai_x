4f453339917e9aabbef8e76b12865ea7f1d066ed
onizuka-38 <bdh9938@gmail.com>
2025-07-16 11:28:06 +0900

    12321312

--- source/11_Flask/ch3_methods/app.py
+++ source/11_Flask/ch3_methods/app.py
@@ -35,5 +35,7 @@ def join():
 def update(name, id, pw, addr):
   return f"{name}님 정보가 수정되었습니다"
 
-if __name__ == "__main__":
-  app.run(debug=True, port=80)
+@app.route("/delete/<id>", methods=["DELETE"])
+def delete(id):
+  # delete from 테이블명 where id = id 를 DBMS에 전송하기
+  return f"{id}님 정보가 삭제되었습니다"
