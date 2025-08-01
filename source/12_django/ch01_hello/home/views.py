f21a8453bc9e7fad47e5b6ae47f931c27fbea4cb
onizuka-38 <bdh9938@gmail.com>
2025-08-01 11:27:42 +0900

    noty

--- source/12_django/ch01_hello/home/views.py
+++ source/12_django/ch01_hello/home/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello, Django!</h1><p>Welcome to the home page.</p>")
+    # return HttpResponse("<h1>Hello, Django!</h1><p>Welcome to the home page.</p>")
+    return render(request,
+                  template_name="home.html",
+                  context={"message": "장고하이ㅋㅋ"})
