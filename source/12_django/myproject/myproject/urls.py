bf0b41e6f59a152dc769fbe999a6723b7da4192f
onizuka-38 <bdh9938@gmail.com>
2025-08-19 17:57:18 +0900

    django

--- source/12_django/myproject/myproject/urls.py
+++ source/12_django/myproject/myproject/urls.py
@@ -24,5 +24,10 @@ urlpatterns = [
     path("", lambda req : redirect("blog:index")),
     path("blog/", include("blog.urls")),
     path("book/", include("book.urls")),
-    path("article/", include("article.urls"))
+    path("article/", include("article.urls")),
 ]
+# 장고는 static은 자동 연결해 주나, media는 개발자가 url과 저장경로를 연결
+from django.conf.urls.static import static
+from . import settings
+urlpatterns += static(settings.MEDIA_URL, # "/media/~~"
+                    document_root = settings.MEDIA_ROOT) # BASE_DIR/_media/ 저장
